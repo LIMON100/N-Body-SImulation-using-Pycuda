7294561e407db3e277dc0f76294cd8a0897765a6
limon100 <mahmudurlimon41@gmail.com>
2020-07-18 16:23:24 +0600

    update file

--- pycuda/Calculation.py
+++ pycuda/Calculation.py
@@ -1,14 +1,3 @@
-#import os
-#if (os.system("cl.exe")):
-#    os.environ['PATH'] += ';'+r"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX64\x64"
-#if (os.system("cl.exe")):
-#    raise RuntimeError("cl.exe still not found, path probably incorrect")
-
-
-
-
-
-
 import pycuda.autoinit
 import pycuda.driver as drv
 from pycuda.compiler import SourceModule
@@ -19,125 +8,103 @@ from MakeTree import qTree, findChild
 import pycuda.gpuarray as gpuarray
 
 
+
 X = 0
 Y = 1
 velx = 2
 vely = 3
 mass = 4
-G = 4.*np.pi**2
-
-dT = 0.01
-BLOCK_SIZE = 64
 
 
 WHITE = (255, 255, 255)
 RED = (255,   0,   0)
+LAWN_GREEN = (124 , 252 , 0)
+
+
+
+
+DenseEvalAcelretion = SourceModule("""
+                    
+                       
+#include<math.h>                     
+
+__global__ void dense_aceleration(float *targetBody , float *body , float *acc , float mag)
+{
+     
+    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    mag = 1. / pow(sqrt((pow((targetBody[0] - body[0]) , 2)) + (pow((targetBody[1] - body[1]) , 2))) , 3);
+    
+    acc[0] += body[4] * ((body[1] - targetBody[0]) * (mag));
+    acc[1] += body[4] * ((body[1] - targetBody[0]) * (mag));
+    
+    
+    //return acc;
+
+}
+""")
+
+
+
+#eval_mod_ac = SourceModule(DenseEvalAcelretion)
+eval_ker_ac = DenseEvalAcelretion.get_function('dense_aceleration')
 
 
 
 
 
-CalculateVelocities = SourceModule("""
-
-#include<math.h>                                   
+DenseEvalVeclocity = SourceModule("""
+                    
 
 #define G 4 * pow((3.1416) , 2)
-#define dT 0.01
+#define timestep 0.1
 
-#define BLOCK_SIZE 128
-                       
-         
+                                         
 
-__device__ float calculate_velocity_change_planet(float* p, float* q , float* acc3)
+__global__ void dense_velocity(float *acc , float *targetBody)
 {
-
     
-    acc3[0] = (dT * G) * ((q[4] / (pow(sqrt(((q[0] - p[0])) * ((q[0] - p[0])) + (q[1] - p[1]) * (q[1] - p[1])) , 3))) * (q[0] - p[0]));
-    acc3[1] = (dT * G) * ((q[4] / (pow(sqrt(((q[0] - p[0])) * ((q[0] - p[0])) + (q[1] - p[1]) * (q[1] - p[1])) , 3))) * (q[1] - p[1]));
+ 
+    targetBody[2] += (G * timestep) * (acc[0]);
+    targetBody[3] += (G * timestep) * (acc[1]);
     
-    return *acc3;
+    
 }
+""")
 
 
 
-__device__ float calculate_velocity_change_block(float* my_planet, float* shared_planets , float* velocities , float* acc2 , float* acc3)
+#eval_mod_ac = SourceModule(DenseEvalAcelretion)
+eval_ker_vt = DenseEvalVeclocity.get_function('dense_velocity')
+
+
+
+
+
+DenseEvalPosition = SourceModule("""
+                    
+#define timestep 0.1
+
+                                         
+
+__global__ void dense_pos(float *body)
 {
-
-    //float velocity [2] = {0.0 , 0.0};
-    //float *tempv[2];
-    
-    
-    for(int i = 0; i < blockDim.x; i++) {
-    
-        *acc2 = calculate_velocity_change_planet(my_planet , (&shared_planets[i]) , acc3);
-        
-        velocities[0] += acc2[0];
-        velocities[1] += acc3[1];
-    }
-
-    return *velocities;
-}
-
-
-
-
-__global__ void update_velocities(float* planets, float* velocities , float planet_size , float* acc1 , float* acc2 , float* acc3)
-{
-
-    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-    
-    float my_planet = planets[thread_id];
-
-    __shared__ float shared_planets[BLOCK_SIZE];
-
-    for(int i = 0; i < planet_size; i += blockDim.x) {
      
-        shared_planets[threadIdx.x] = planets[i + threadIdx.x];
-        
-        __syncthreads();
-        
-        //float *tempv[2];
-        *acc1 = calculate_velocity_change_block(&my_planet, shared_planets , velocities , acc2 , acc3);
-        
-        //planets[thread_id]->velocities[0] += acc[0];
-        //planets[thread_id]->velocities[1] += acc[1];
-        
-        velocities[thread_id] += acc1[0];
-        velocities[thread_id] += acc1[1];
-        
-        __syncthreads();
-    }
+     body[0] += (body[2]) * timestep;
+     body[1] += (body[3]) * timestep;
     
-    return;
+    
 }
-
 """)
 
 
 
-#eval_mod_ac = SourceModule(CalculateVelocities)
-eval_ker_cal = CalculateVelocities.get_function("update_velocities")
+#eval_mod_ac = SourceModule(DenseEvalAcelretion)
+eval_ker_pos = DenseEvalPosition.get_function('dense_pos')
 
 
 
-DensePosition = SourceModule("""
-                             
-#define dT 0.01
-                             
-__global__ void update_positions(float* planets, float* velocities)
-{
 
-    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-
-    planets[thread_id] += (velocities[thread_id]) * (dT);
-    planets[thread_id] += (velocities[thread_id]) * (dT);
-    
-}
-
-""")
-
-
-eval_ker_pos = DensePosition.get_function("update_positions")
 
 
 
@@ -147,117 +114,145 @@ class point:
         self.y = y
 
 
-
 class body:
     """loc in units of AU
         mass in solar mass units
         velociy in AU/yr
     """
 
-    
-    def __init__(self, position , velocity):
-        
+    def __init__(self,  position, velocity):
+
         #self.loc = loc
         #self.mass = mass
         #self.vel = vel
         #self.name = name
         #self.colour = colour
         #self.dispMass = dispMass
-        
+
         #self.mass = mass
         self.position = position
         self.velocity = velocity
-        
-
 
 
 class euler_integrator:
 
     def __init__(self, timestep, bodies, screen, size, mag):
-        
+
         self.timestep = timestep
         self.bodies = bodies
         self.screen = screen
         self.size = size
         self.mag = mag
         
+        self.mag = np.float32(mag)
+        
+        self.block = (128 , 1 , 1)
+        self.grid = (8 , 1 , 1)
+        
+     
         self.bodies = np.float32(bodies)
         self.bodies_gpu = gpuarray.to_gpu(bodies)
         self.bodies_output_gpu = gpuarray.empty_like(self.bodies_gpu)
         
-        
-        self.bodies_len = len(self.bodies)
-        self.bodies_len = np.float32(self.bodies_len)
-        
-        
-        self.velocities = np.float32(bodies)
-        self.velocities_gpu = gpuarray.to_gpu(self.velocities)
-        self.velocities_output_gpu = gpuarray.empty_like(self.velocities_gpu)
+        self.acc = [0 , 0]
+        self.acc = np.float32(self.acc)
+        self.acc_gpu = gpuarray.to_gpu(self.acc)
+        self.acc_output_gpu = gpuarray.empty_like(self.acc_gpu)
         
         
-        self.acc1 = [0 , 0]
-        self.acc1 = np.float32(self.acc1)
-        self.acc1_gpu = gpuarray.to_gpu(self.acc1)
+        
+
+    def calcAccleration(self, bodyIdx):    
+        
+        targetBody = self.bodies[bodyIdx]
+        
+        targetBody = np.float32(targetBody)
+        targetBody_gpu = gpuarray.to_gpu(targetBody)
+        targetBody_output_gpu = gpuarray.empty_like(targetBody_gpu)
+   
+        
+        for idx, body in enumerate(self.bodies):
+            
+            body = np.float32(body)
+            body_gpu = gpuarray.to_gpu(body)
+            body_output_gpu = gpuarray.empty_like(body_gpu)
+            
+            if idx != bodyIdx:
+                               
+                
+                eval_ker_ac(targetBody_gpu , body_gpu , self.acc_gpu , self.mag , block = self.block , grid = self.grid)
         
         
-        self.acc2 = [0 , 0]
-        self.acc2 = np.float32(self.acc2)
-        self.acc2_gpu = gpuarray.to_gpu(self.acc2)
-        
-        self.acc3 = [0 , 0]
-        self.acc3 = np.float32(self.acc3)
-        self.acc3_gpu = gpuarray.to_gpu(self.acc3)
+        aclarte_out = self.acc_gpu.get()
+        #print(aclarte_out)
+        return aclarte_out
         
         
-        self.block = (64 , 1 , 1)
-        self.grid = (int(np.ceil(len(self.bodies) / 32)), 1,1)
-        #self.grid = (8 , 1 , 1)
-        
-    
 
     def calcVelocity(self):
         
         for idx, targetBody in enumerate(self.bodies):
-
             
-            eval_ker_cal(self.bodies_gpu , self.velocities_output_gpu  , self.bodies_len , self.acc1_gpu , self.acc2_gpu , self.acc3_gpu ,  block = self.block , grid = self.grid)
+            acc = self.calcAccleration(idx)
+            acc = np.float32(acc)
+            acc_gpu = gpuarray.to_gpu(acc)
+            
+            
+            targetBody = np.float32(targetBody)
+            targetBody_gpu = gpuarray.to_gpu(targetBody)
+            #body_output_gpu = gpuarray.empty_like(targetBody_gpu)
+            
+            eval_ker_vt(acc_gpu , targetBody_gpu , block = self.block , grid = self.grid)
+            
+        
+        
+        ac_out = acc_gpu.get()
+        #print(ac_out)
+            
+            
             
             
 
     def calcPosition(self):
-       
-        
-        body = []
+        """
+            updates postions and draws body on pygame screen
+            also calculates quadtree for the collection of bodies
+            returns list of children in quadtree for drawing
+        """
         qt = qTree(1, self.size[1], self.size[0])
         
         for body in self.bodies:
             
-            #update korte hobe with body die.
+            #body[0] += (body[2]) * self.timestep;
+            #body[1] += (body[3]) * self.timestep;
+            
+            #print(body)
             
             body = np.float32(body)
             body_gpu = gpuarray.to_gpu(body)
             
+            body_gpu_output = gpuarray.empty_like(body_gpu)
             
-            eval_ker_pos(body_gpu , self.velocities_gpu  , block = self.block , grid = self.grid)
-            
-            #body[X] += body[velx] * self.timestep
-            #body[Y] += body[vely] * self.timestep
+            eval_ker_pos(body_gpu , block = self.block , grid = self.grid)
             
             body = body_gpu.get()
+            print(body)
             
             qt.addPoint(body[X], body[Y], body[mass])
             
-            pygame.draw.circle(self.screen,  RED , (self.size[2] + int(body[X] * self.mag), self.size[3] + int(body[Y] * self.mag)) , 2)
+            pygame.draw.circle(self.screen,  LAWN_GREEN , (self.size[2] + int(
+                body[X] * self.mag), self.size[3] + int(body[Y] * self.mag)), 2)
             
             
-            
+
         qt.subDivide()
         c = findChild(qt.root)
         del qt
         return c
     
+    
 
-    def doStep(self):
+    def doStep(self):        
         self.calcVelocity()
         c = self.calcPosition()
         return c
@@ -266,6 +261,7 @@ class euler_integrator:
 def runSim(integrator, steps, bodies, Histo):
 
     c = integrator.doStep()
+    
     if steps % 200 == 0:
         for idx, bodyLoc in enumerate(Histo):
             try:
